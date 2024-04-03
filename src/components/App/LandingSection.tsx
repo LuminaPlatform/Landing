8a64e79c72641f166863383d1bb8ab523e428e8b
mohammadbagheri228@gmail.com <mohammadbagheri228@gmail.com>
2024-04-03 20:45:58 +0330

    fix: changed landing section shadows

--- src/components/App/LandingSection.tsx
+++ src/components/App/LandingSection.tsx
@@ -1,11 +1,13 @@
 import { Box, Button, GridItem, HStack, Img, SimpleGrid, Text, VStack, Link } from '@chakra-ui/react'
 
-type LandingSection = Record<'imgSrc' | 'description' | 'title' | 'exploreButtonTitle' | 'href' | 'id', string>
+type LandingSection = Record<'imgSrc' | 'description' | 'title' | 'exploreButtonTitle' | 'href' | 'id', string> & {
+  hasShadow?: boolean
+}
 
-const LandingSection = ({ imgSrc, title, description, exploreButtonTitle, href, id }: LandingSection) => (
+const LandingSection = ({ imgSrc, title, description, exploreButtonTitle, href, id, hasShadow }: LandingSection) => (
   <SimpleGrid
     id={id}
-    _odd={{
+    _even={{
       '&>div:first-of-type': {
         order: { md: 1 },
       },
@@ -14,8 +16,68 @@ const LandingSection = ({ imgSrc, title, description, exploreButtonTitle, href, 
     gap="24px"
     columns={[1, null, 2]}
   >
-    <GridItem width="full">
+    <GridItem position="relative" width="full">
       <Img src={imgSrc} />
+      {hasShadow && (
+        <>
+          <Box
+            zIndex={-1}
+            opacity="0.2"
+            rounded="full"
+            transform="rotate(-30deg)"
+            as="span"
+            position="absolute"
+            right={{ base: '80px', md: '0' }}
+            top={{ base: '120px', md: '80px' }}
+            width="70px"
+            height="150px"
+            background="rgba(102, 0, 204, 0.5)"
+            boxShadow={`0 0 ${50}px ${50}px rgba(102, 0, 204, 0.5)`}
+          />
+          <Box
+            zIndex={-1}
+            opacity="0.2"
+            rounded="full"
+            transform="rotate(-30deg)"
+            as="span"
+            position="absolute"
+            right={{ base: '80px', md: '0' }}
+            top={{ base: '120px', md: '80px' }}
+            width="70px"
+            height="150px"
+            background="rgba(43, 215, 228, 0.5)"
+            boxShadow={`0 0 ${100}px ${100}px rgba(43, 215, 228, 0.5)`}
+          />
+          <Box
+            zIndex={-1}
+            opacity="0.2"
+            rounded="full"
+            transform="rotate(-30deg)"
+            as="span"
+            position="absolute"
+            left={{ base: '150px', md: '100px' }}
+            bottom={{ base: '100px', md: '150px' }}
+            width="70px"
+            height="150px"
+            background="rgba(102, 0, 204, 0.5)"
+            boxShadow={`0 0 ${50}px ${50}px rgba(102, 0, 204, 0.5)`}
+          />
+          <Box
+            zIndex={-1}
+            opacity="0.2"
+            rounded="full"
+            transform="rotate(-30deg)"
+            as="span"
+            position="absolute"
+            left={{ base: '150px', md: '100px' }}
+            bottom={{ base: '100px', md: '150px' }}
+            width="70px"
+            height="150px"
+            background="rgba(43, 215, 228, 0.5)"
+            boxShadow={`0 0 ${100}px ${100}px rgba(43, 215, 228, 0.5)`}
+          />
+        </>
+      )}
     </GridItem>
     <GridItem as={VStack} justifyContent="center">
       <Text width="full" textAlign="left" color="gray.40" pb="24px" fontSize="48px" fontWeight="bold">
